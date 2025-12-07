ab669b1c04acb609b6abc5d0f29136d789d1e959
quietdw <1044569102@qq.com>
2025-12-07 21:25:33 +0800

    feat: enhance health monitoring component by adding error handling for disconnect and health monitoring actions; update device name display with computed property

--- src/pages/mine/components/health-monitoring.vue
+++ src/pages/mine/components/health-monitoring.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { useWatcher } from 'alova/client'
+import type { Transformer_devicetransformer } from '@/api/globals'
 import { getFileUrl } from '@/utils/file'
 import Apis from '@/api'
 import router from '@/router'
 
 const { userinfo } = storeToRefs(useUserInfoStore())
-
+const globalToast = useGlobalToast()
 const deviceId = computed(() => userinfo.value?.default_device_id || 0)
-const { data, loading, error, send } = useWatcher(
+const { data: rawData } = useWatcher(
   () => Apis.device.get_api_device_id(
     {
       pathParams: {
@@ -26,6 +27,19 @@ const { data, loading, error, send } = useWatcher(
     },
   },
 )
+
+// 使用计算属性进行类型转换
+const data = computed(() => rawData.value as { data?: Transformer_devicetransformer, msg?: string } | undefined)
+
+function handleDisconnect() {
+  console.log('handleDisconnect')
+  globalToast.error('断开连接失败')
+}
+
+function handleHealthMonitoring() {
+  globalToast.error('健康监测失败')
+  console.log('handleHealthMonitoring')
+}
 </script>
 
 <script lang="ts">
@@ -46,7 +60,7 @@ export default {
     <view v-if="deviceId" class="arr-card bg-#fff">
       <view class="flex items-center justify-between">
         我的设备
-        <wd-button size="small" custom-class="!h-40rpx rounded-16rpx !bg-#EBEBEB">
+        <wd-button size="small" custom-class="!h-40rpx rounded-16rpx !bg-#EBEBEB" @click="handleDisconnect">
           <text class="t-s text-#B2B2B2">
             断开连接
           </text>
@@ -57,13 +71,20 @@ export default {
         <image :src="getFileUrl('/img/device.svg')" class="h-176rpx w-176rpx" />
         <view class="min-w-0 flex flex-1 flex-col gap-24rpx">
           <view class="t-s flex">
-            apple watch series 11
-            <wd-icon name="arrow-right" custom-class="ml-auto" />
+            {{ data?.data?.device_name }}
+            <wd-icon
+              name="arrow-right" custom-class="ml-auto" @click="router.push({
+                name: 'bind-device',
+                params: {
+                  deviceId: String(data?.data?.id || ''),
+                },
+              })"
+            />
           </view>
           <view class="t-s text-#999">
             已同步所有数据
           </view>
-          <wd-button size="small" custom-class="!h-50rpx !w-140rpx rounded-16rpx !bg-[var(--primary-color)]">
+          <wd-button size="small" custom-class="!h-50rpx !w-140rpx rounded-16rpx !bg-[var(--primary-color)]" @click="handleHealthMonitoring">
             健康监测
           </wd-button>
         </view>
